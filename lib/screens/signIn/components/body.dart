86d87d0792e1aa60abbc36d9600f63ed319ec38f
Ankit Jailwal <jailwalankit@gmail.com>
2021-05-25 01:08:14 +0530

    Added Text for testing in signIn.dart

--- lib/screens/signIn/components/body.dart
+++ lib/screens/signIn/components/body.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(child: Text("Space for SingIn screen")),
+    );
   }
 }
