00f6c5edda3b36801a852123431e163e0c0b18d8
Ankit Jailwal <jailwalankit@gmail.com>
2021-05-25 00:52:47 +0530

    Changing name of main class in widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:emergencyapp/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(EmergencyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
