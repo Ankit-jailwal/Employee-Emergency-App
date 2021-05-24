144448792b0a10d3a8a252b23a239890d83544d5
Ankit Jailwal <jailwalankit@gmail.com>
2021-05-25 00:09:45 +0530

    Created signIn class in signIn.dart

--- lib/screens/signIn/signIn.dart
+++ lib/screens/signIn/signIn.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+//import 'package:move_to_background/move_to_background.dart';
+import 'package:emergencyapp/screens/signIn/components/body.dart';
 
+class signIn extends StatelessWidget {
+  static String routeName = "/signIn";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff262833),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+            onPressed: (){
+              // MoveToBackground.moveTaskToBack();
+            },
+            icon: Icon(Icons.arrow_back,color: Colors.white,size: 30,)),
+      ),
+      // body: Body()
+    );
+  }
+}
