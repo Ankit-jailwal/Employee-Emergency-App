f3c30809a8213b2811b4d5a62aa65298585880de
Ankit Jailwal <jailwalankit@gmail.com>
2021-05-28 04:27:16 +0530

    Removed Login text and implemented disableScrollGlow

--- lib/screens/signIn/components/body.dart
+++ lib/screens/signIn/components/body.dart
@@ -1,3 +1,4 @@
+import 'package:emergencyapp/components/disbaleScrollGlow.dart';
 import 'package:emergencyapp/constant.dart';
 import 'package:flutter/material.dart';
 
@@ -34,40 +35,18 @@ class _BodyState extends State<Body> {
               width: size.width * 0.4,
             ),
           ),
-          SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "LOGIN",
-                  style: TextStyle(fontWeight: FontWeight.w500,fontFamily: 'hacked',fontSize: 40,letterSpacing: 2.5,color:Color(0xFF6F35A5)),
-                ),
-                SizedBox(height: size.height * 0.03),
-                Image.asset(
-                  "assets/images/login.png",
-                  height: size.height * 0.35,
-                ),
-                SizedBox(height: size.height * 0.03),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10),
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                width: size.width * 0.8,
-                decoration: BoxDecoration(
-                  color: kPrimaryLightColor,
-                  borderRadius: BorderRadius.circular(29),
-                ),
-                child:  TextField(
-                    cursorColor: kPrimaryColor,
-                    decoration: InputDecoration(
-                      icon: Icon(
-                        Icons.person,
-                        color: kPrimaryColor,
-                      ),
-                      hintText: "Your Email",
-                      border: InputBorder.none,
-                    ),
-                ),
-              ),
+          ScrollConfiguration(
+            behavior: disableScrollGlow(),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(height: size.height * 0.03),
+                  Image.asset(
+                    "assets/images/login.png",
+                    height: size.height * 0.35,
+                  ),
+                  SizedBox(height: size.height * 0.03),
                 Container(
                   margin: EdgeInsets.symmetric(vertical: 10),
                   padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
@@ -77,40 +56,61 @@ class _BodyState extends State<Body> {
                     borderRadius: BorderRadius.circular(29),
                   ),
                   child:  TextField(
-                    obscureText: true,
-                    cursorColor: kPrimaryColor,
-                    decoration: InputDecoration(
-                      hintText: "Password",
-                      icon: Icon(
-                        Icons.lock,
-                        color: kPrimaryColor,
+                      cursorColor: kPrimaryColor,
+                      decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.person,
+                          color: kPrimaryColor,
+                        ),
+                        hintText: "Your Email",
+                        border: InputBorder.none,
                       ),
-                      suffixIcon: Icon(
-                        Icons.visibility,
-                        color: kPrimaryColor,
+                  ),
+                ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    width: size.width * 0.8,
+                    decoration: BoxDecoration(
+                      color: kPrimaryLightColor,
+                      borderRadius: BorderRadius.circular(29),
+                    ),
+                    child:  TextField(
+                      obscureText: true,
+                      cursorColor: kPrimaryColor,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        icon: Icon(
+                          Icons.lock,
+                          color: kPrimaryColor,
+                        ),
+                        suffixIcon: Icon(
+                          Icons.visibility,
+                          color: kPrimaryColor,
+                        ),
+                        border: InputBorder.none,
                       ),
-                      border: InputBorder.none,
+                    ),
+                  ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  width: size.width * 0.8,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(29),
+                    child: FlatButton(
+                      onPressed: (){},
+                      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                      color: kPrimaryColor,
+                      child: Text(
+                        "LOGIN",
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
                   ),
                 ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10),
-                width: size.width * 0.8,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(29),
-                  child: FlatButton(
-                    onPressed: (){},
-                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-                    color: kPrimaryColor,
-                    child: Text(
-                      "LOGIN",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
+                  SizedBox(height: size.height * 0.03),
+                ],
               ),
-                SizedBox(height: size.height * 0.03),
-              ],
             ),
           ),
         ],
