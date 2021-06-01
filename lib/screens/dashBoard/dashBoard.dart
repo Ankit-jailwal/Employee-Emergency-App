b26a11ea937f6d556b0075270ba1705838479427
Ankit Jailwal <jailwalankit@gmail.com>
2021-06-02 00:04:35 +0530

    Adding SafeArea in dashBoard.dart

--- lib/screens/dashBoard/dashBoard.dart
+++ lib/screens/dashBoard/dashBoard.dart
@@ -6,18 +6,20 @@ class dashBoard extends StatelessWidget {
   static String routeName = "/dashBoard";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: kPrimaryColor.withOpacity(0.7),
-          title: Text("Dashboard", style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.w400),),
-          leading: IconButton(
-              onPressed: (){
-                Navigator.pop(context);
-              },
-              icon: Icon(Icons.arrow_back,color: Colors.white,size: 30,)),
-        ),
-        body: Body()
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: kPrimaryColor.withOpacity(0.7),
+            title: Text("Dashboard", style: TextStyle(color: Colors.white,fontSize: 20,fontWeight: FontWeight.w400),),
+            leading: IconButton(
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+                icon: Icon(Icons.arrow_back,color: Colors.white,size: 30,)),
+          ),
+          body: Body()
+      ),
     );
   }
 }
